370b96f4d020e55432f8093fc148eb460df5a18b
Driele Neves Ribeiro <t-drneve@microsoft.com>
2018-03-13 18:24:34 -0700

    Tests for .NET Runtime MSIs

---
+++ src/pkg/packaging/windows/test-installer/Msi.Tests/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Msi.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string msiFile = Environment.GetEnvironmentVariable("HOST_MSI");
+            Console.WriteLine(msiFile);
+        }
+    }
+}
---
+++ src/pkg/packaging/windows/test-installer/testmsi.ps1
@@ -0,0 +1,67 @@
+# Copyright (c) .NET Foundation and contributors. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+param(
+    [string]$InputMsi,
+    [string]$TestDir
+)
+
+$RepoRoot = Convert-Path "$PSScriptRoot\..\..\..\..\.."
+$CommonScript = "$RepoRoot\tools-local\scripts\common\_common.ps1"
+if(-Not (Test-Path "$CommonScript"))
+{
+    Exit -1
+} 
+. "$CommonScript"
+
+
+Write-Output "Running tests for MSI installer at $inputMsi."
+
+if(!(Test-Path $InputMsi))
+{
+    throw "$inputMsi not found" 
+}
+
+$testName = "Msi.Tests"
+$testProj="$PSScriptRoot\$testName\$testName.csproj"
+$testBin="$TestDir\$testName"
+
+$toolsLocalPath = Join-Path $PSScriptRoot "Tools"
+$dotNetExe = Join-Path $toolsLocalPath "dotnetcli\dotnet.exe"
+
+try {
+    & $dotNetExe restore $testProj | Out-Host
+
+    if($LastExitCode -ne 0)
+    {
+        throw "dotnet restore failed with exit code $LastExitCode."     
+    }
+
+    & $dotNetExe publish --output $testBin $testProj | Out-Host
+
+    if($LastExitCode -ne 0)
+    {
+        throw "dotnet publish failed with exit code $LastExitCode."     
+    }
+
+    & $dotNetExe $testName.dll
+
+   
+	$runTest = Join-Path $testBin $testName
+	$runTest = $runTest + ".dll"
+
+        Write-Output "Running installer tests"
+
+        $HOST_MSI = [System.IO.Path]::GetFileName($InputMsi)
+
+	& $dotNetExe $runTest
+
+    	if($LastExitCode -ne 0)
+    	{
+        	throw "dotnet run-dll failed with exit code $LastExitCode."     
+    	}
+       
+}
+
+Exit 0
+
